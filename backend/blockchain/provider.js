e67e27e38b6e5210312b2c559ab7f76ca56040ec
jrastit <jerome.rastit@libertysurf.fr>
2024-03-17 15:06:52 +0100

    fetch nft metadata

--- backend/blockchain/provider.js
+++ backend/blockchain/provider.js
@@ -4,15 +4,15 @@ const { ethers } = require("ethers"); // assuming commonjs
 
 const fs = require("fs");
 provider = getDefaultProvider("https://polygon-pokt.nodies.app");
-provider = getDefaultProvider("http://localhost:8545");
-provider = getDefaultProvider("https://spicy-rpc.chiliz.com/");
-provider = getDefaultProvider("https://arbitrum-nova.drpc.org");
+//provider = getDefaultProvider("http://localhost:8545");
+//provider = getDefaultProvider("https://spicy-rpc.chiliz.com/");
+//provider = getDefaultProvider("https://arbitrum-nova.drpc.org");
 // provider = getDefaultProvider("https://nova.arbitrum.io/rpc");
 // provider = getDefaultProvider("https://arbitrum-nova.public.blastapi.io");
 //provider = getDefaultProvider("mainnet");
-const abi = JSON.parse(fs.readFileSync("abi.json"));
+const token = JSON.parse(fs.readFileSync("token.json"));
 
-let iface = new ethers.Interface(abi);
+let iface = new ethers.Interface(token.output.abi);
 
 // This is the "transfer event" topic we want to watch.
 const mintTopic = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
@@ -21,7 +21,6 @@ const zeroTopic = "0x00000000000000000000000000000000000000000000000000000000000
 const zeroAddress = "0x0000000000000000000000000000000000000000"
 module.exports = {
     provider,
-    abi,
     iface,
     mintTopic,
     zeroTopic,
--- backend/blockchain/watch_node.js
+++ backend/blockchain/watch_node.js
@@ -1,6 +1,65 @@
 const e = require("express");
 const logger = require("logger");
 const { provider, iface, mintTopic, zeroAddress } = require("../blockchain/provider");
+const { ethers } = require("ethers"); // assuming commonjs
+const axios = require("axios");
+
+function getJsonFromURL(url) {
+    axios.get(url).then(response => {
+                        const json = response.data;
+                        console.log("JSON content:", json);
+                        // Do something with the JSON content
+                    })
+                    .catch(error => {
+                        console.error("Error fetching JSON:", error);
+                    });
+}
+
+function getURI(provider, contractAddress, nftId) {
+    let data = iface.encodeFunctionData('tokenURI', [nftId]);
+
+    
+    // Make the call
+    provider.call({ to: contractAddress, data: data }).then((result) => {
+        // Decode the result
+        let uri_obj = iface.decodeFunctionResult('tokenURI', result);
+        uri = uri_obj.toString();
+        //console.log("NFT URI:", uri);
+        if (typeof uri === "string") {
+            if (uri.startsWith("data:")) {
+                console.log("is data URI");
+                data = uri.substring(5);
+                if (data.startsWith("application/json;base64,")) {
+                    console.log("is json 64");
+                    json_b64_data = data.substring(24);
+                    let json_data = Buffer.from(json_b64_data, 'base64').toString('utf-8');
+                    let json = JSON.parse(json_data);
+                    //console.log("NFT JSON:", json);
+                }
+            } else if (uri.startsWith("ipfs://")) {
+                const ipfsHash = uri.substring(7);
+                const httpsURI = `https://ipfs.io/ipfs/${ipfsHash}`;
+                
+                console.log("is https URI:", httpsURI);
+                getJsonFromURL(httpsURI);
+            } else if (uri.startsWith("https://")) {
+                console.log("is https URI");
+                getJsonFromURL(uri);
+            } else if (uri.startsWith("http://")) {
+                console.log("is http URI");
+                getJsonFromURL(uri);
+            } else {
+                console.error("Invalid NFT URI:", uri.substring(10));
+            }
+        } else {
+            console.error("Invalid NFT URI type:", typeof uri);
+        }
+
+    }).catch((error) => {
+        console.error("Error getting NFT URI:", error);
+    });
+}
+
 function watchNode(provider) {
     provider.on("block", async (n) => {
         const block = await provider.getBlock(n);
@@ -19,6 +78,7 @@ function watchNode(provider) {
                                     let mylog = iface.parseLog(log);
                                     if (mylog.args[0] === zeroAddress) {
                                         console.log(log.address, "Mint", mylog.args[1], mylog.args[2]);
+                                        getURI(provider, log.address, mylog.args[2]);
                                     } else {
                                         console.log(log.address, "Transfer", mylog.args[0], mylog.args[1], mylog.args[2]);
                                     }
